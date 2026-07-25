b096d70ba29f1ad08ce55b7b57a902ab32e3308c
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-07-25 22:24:37 +0530

    Add client env example

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://eventora-mern-26rr.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
